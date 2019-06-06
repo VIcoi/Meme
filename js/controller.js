b2fc7a989440cc93730ae3d43058476d36807940
vico <vicoida1@gmail.com>
2019-06-06 11:56:54 +0300

    add js controller

---
+++ js/controller.js
@@ -0,0 +1,62 @@
+'use strict'
+
+
+
+let canvas
+let ctx
+
+let currElement = 'arc'
+
+function changeEl(elName) {
+    currElement = elName
+}
+
+function init() {
+    canvas = document.querySelector('#my-canvas');
+    ctx = canvas.getContext('2d')
+
+    // canvas.width = window.innerWidth - 50
+    // canvas.height = window.innerHeight - 100
+
+    // ctx.fillStyle = 'green'
+    // ctx.save()
+    // drawTriangle()
+    // ctx.restore()
+    // drawRect()
+    // drawArc()
+    // clearCanvas()
+
+    // drawImg()
+    // drawText('HELLO CANVAS')
+}
+
+function draw(ev) {
+    ctx.save();
+    drawImg();
+    const {offsetX, offsetY} = ev
+    switch (currElement) {
+        case 'triangle':
+            drawTriangle(offsetX, offsetY)
+            break;
+        case 'rect':
+            drawRect(offsetX, offsetY)
+            break;
+        case 'text':
+            drawText('test',offsetX, offsetY)
+            break;
+        case 'arc':
+            drawArc(offsetX, offsetY)
+            break;
+    }
+    ctx.restore()
+}
+
+
+
+
+
+function drawImg() {
+    const img = document.querySelector('img');
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+
+}
